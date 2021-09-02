9f40f4323d3186dcf966e800e764cbe4f60e417c
yusukenamiki <yusukenamiki.g@gmail.com>
2021-09-03 02:48:43 +0900

    Refile.secret_keyの設定直後

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd5cb164b176042e180f7ca000ec6276cec4ce22fe1e10eec3ad0a87a8dd8c7c7ac796e4eff5bf1af4e0ca06f368ab565ad22b004bbe634f86c3054226e11fbe7'
