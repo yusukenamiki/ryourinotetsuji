549935d243745e9e996ce7c5a9561db32d862218
yusukenamiki <yusukenamiki.g@gmail.com>
2021-09-09 14:42:42 +0900

    他人のユーザー情報の編集画面へのアクセス制限実装直後

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: '不正なアクセスです。'
+    end
   end
 
   def update
