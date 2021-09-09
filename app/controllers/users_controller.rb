c7e2373e0a47e3694c8bc919eb3c33b7e5c8957f
yusukenamiki <yusukenamiki.g@gmail.com>
2021-09-09 17:29:52 +0900

    フラッシュメッセージの実装直後

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user)
+      redirect_to user_path(@user), notice: '更新に成功しました。'
     else
       render :edit
     end
