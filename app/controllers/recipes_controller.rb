9dd00c71aa20d4ca2934f88e8fe00c95754a1de4
yusukenamiki <yusukenamiki.g@gmail.com>
2021-09-09 16:03:15 +0900

    バリデーションの実装直後

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,8 +15,11 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
-    @recipe.save
-    redirect_to recipe_path(@recipe)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -28,8 +31,11 @@ class RecipesController < ApplicationController
 
   def update
     @recipe = Recipe.find(params[:id])
-    @recipe.update(recipe_params)
-    redirect_to recipe_path(@recipe)
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
   end
 
   def destroy
