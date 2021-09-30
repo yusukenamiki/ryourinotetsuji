f21589704138756870babee591f24408649b4204
yusukenamiki <yusukenamiki.g@gmail.com>
2021-09-30 21:58:01 +0900

    ゲストユーザーの情報を変更直後

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: :destroy
 
   def check_guest
-    if resource.email == 'guest@example.com'
+    if resource.email == 'guestuser@example.com'
       redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
     end
   end
