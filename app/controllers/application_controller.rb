8bffe94a7314c25c9bf201ddf243e66d06142e36
yusukenamiki <yusukenamiki.g@gmail.com>
2021-10-01 00:55:39 +0900

    ゲストユーザーがメールアドレス・パスワードを変更できないようにした直後

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def check_guest
+    email = resource&.email || params[:user][:email].downcase
+    if email == 'guestuser@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの編集・削除はできません。'
+    end
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
