809d893e1434a3db61077479663a8696e2216794
yusukenamiki <yusukenamiki.g@gmail.com>
2021-10-02 13:39:22 +0900

    ユーザー一覧画面にページネーションの実装直後

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @users = User.all
+    @users = User.page params[:page]
   end
 
   def show
