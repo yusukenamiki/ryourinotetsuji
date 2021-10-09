72fc0be887367e7e9dd4de888c5028cf9edb1114
yusukenamiki <yusukenamiki.g@gmail.com>
2021-10-09 15:54:00 +0900

    アソシエーション完了直後

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :recipes, dependent: :destroy
-
+  has_many :favorites, dependent: :destroy
   validates :username, presence: true
 
   def self.guest
