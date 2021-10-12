46d40bd3ae6d1bcecf16733f0df299c97c4d49f3
yusukenamiki <yusukenamiki.g@gmail.com>
2021-10-13 03:36:07 +0900

    「実装1」終了後

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  attachment :profile_image
+
   has_many :recipes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :relationships, foreign_key: :following_id
+  has_many :followings, through: :relationships, source: :follower
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :following
+
   validates :username, presence: true
+
+  attachment :profile_image
 
   def self.guest
     find_or_create_by(username: "ゲストユーザー", email: 'guestuser@example.com') do |user|
