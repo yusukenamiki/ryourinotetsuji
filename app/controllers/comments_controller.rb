1b5c99850689902a3aecdb5bc1b013dd42f621af
yusukenamiki <yusukenamiki.g@gmail.com>
2021-10-19 00:43:18 +0900

    コントローラーの記述終了後

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = current_user.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to [@recipe]
+    else
+      render 'recipes/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(recipe_id: params[:recipe_id])
+  end
 end
