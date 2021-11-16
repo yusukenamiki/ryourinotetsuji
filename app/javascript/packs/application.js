35fd3aa2aa9c563d720193b856ec1009ab769577
yusukenamiki <yusukenamiki.g@gmail.com>
2021-11-17 02:09:44 +0900

    画面遷移するとドロップダウンが動かなくなる為、turbolinksを無効化した

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@
 //= require popper
 //= require bootstrap-sprockets
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
