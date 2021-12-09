297e18378fadaf80cf681e05eb0dc198ddbec739
yusukenamiki <yusukenamiki.g@gmail.com>
2021-12-10 01:57:56 +0900

    1ページあたりに表示するデータの数を元に戻した

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.default_per_page = 1
+  config.default_per_page = 6
   # config.max_per_page = nil
   config.window = 0
   config.outer_window = 1
