aff9752560e0bd8e155db9f2846ecd5a460d73fa
yusukenamiki <yusukenamiki.g@gmail.com>
2022-01-16 19:56:47 +0900

    パスワードを忘れた場合画面の送信ボタンをクリックした時、noticeのリダイレクト先を変更とフラッシュメッセージを表示

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,3 +1,7 @@
 class Users::PasswordsController < Devise::PasswordsController
   before_action :check_guest, only: :create
+
+  def after_sending_reset_password_instructions_path_for(resource)
+    new_password_path(resource_name)
+  end
 end
