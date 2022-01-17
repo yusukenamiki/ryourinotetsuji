79d0ae187c32b2feda7d171e49bc31f82819f4a2
yusukenamiki <yusukenamiki.g@gmail.com>
2022-01-18 03:02:05 +0900

    パスワードを変更した時のnoticeのリダイレクト先をマイページに変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
 
-  protected
-
-    def after_update_path_for(resource)
-      edit_user_registration_path
-    end
+  def after_update_path_for(resource)
+    user_path(current_user)
+  end
 end
