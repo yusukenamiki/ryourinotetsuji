9564b184e80e1ab8da7448c8e1626cd56e7abd0d
yusukenamiki <yusukenamiki.g@gmail.com>
2022-01-18 02:43:57 +0900

    パスワードを変更した時のnoticeのリダイレクト先を変更 ( ERに記載の通り丸々コピー&ペースト )

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
+
+  protected
+
+    def after_update_path_for(resource)
+      edit_user_registration_path
+    end
 end
