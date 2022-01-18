a93df857ba22b38a2bf0695432cfce3ba4035fe1
yusukenamiki <yusukenamiki.g@gmail.com>
2022-01-18 23:49:13 +0900

    文言変更 ( プロフィールを編集 → プロフィールを変更 )

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user), notice: 'プロフィールを編集しました。'
+      redirect_to user_path(@user), notice: 'プロフィールを変更しました。'
     else
       render :edit
     end
