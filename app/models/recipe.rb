e9982d6800acff1a145e5a7edcf975f549ca222a
yusukenamiki <yusukenamiki.g@gmail.com>
2022-01-31 21:19:36 +0900

    親モデルを削除する際に、その親モデルに紐づく「コメント」も一緒に削除できるように修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   attachment :image
 
   has_many :favorites, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   with_options presence: true do
     validates :title
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :recipes, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :relationships, foreign_key: :following_id
   has_many :followings, through: :relationships, source: :follower
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
