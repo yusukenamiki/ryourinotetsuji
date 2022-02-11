ab05932fdfd840b04b6c1d826cf45c45cba75046
yusukenamiki <yusukenamiki.g@gmail.com>
2022-02-12 02:50:12 +0900

    モデルにバリデーションを設定 ( レシピ名・作り方 )

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,8 +4,8 @@ class Recipe < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 2200 }
   validates :image, presence: { message: 'を選択してください。' }, file_size: { less_than: 5.megabytes }
 
   attachment :image, type: :image
