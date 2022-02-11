92460ea32144fb2c620415a4dd5fc74e8276f1ba
yusukenamiki <yusukenamiki.g@gmail.com>
2022-02-11 20:01:36 +0900

    with_optionsを個別に分解

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,11 +4,9 @@ class Recipe < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  with_options presence: true do
-    validates :title
-    validates :body
-    validates :image, presence: { message: 'を選択してください。' }, file_size: { less_than: 5.megabytes }
-  end
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true, presence: { message: 'を選択してください。' }, file_size: { less_than: 5.megabytes }
 
   attachment :image, type: :image
 end
