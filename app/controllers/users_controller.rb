9dd83b8191c7176e9aa0cf1788bf0a73c5f53820
yusukenamiki <yusukenamiki.g@gmail.com>
2022-03-06 14:42:34 +0900

    app/views/users/index.html.erbの中で、app/controllers/users_controller.rbのdef indexの中身が使われなくなったので、中身を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    if user_signed_in?
-      @users = User.where.not(id: current_user.id).order("created_at DESC").page(params[:page]).per(9)
-    else
-      @users = User.order("created_at DESC").page(params[:page]).per(9)
-    end
   end
 
   def followings
