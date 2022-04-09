4f7dfbbd922a2f6f8707254150b6feb99c12fbf4
yusukenamiki <yusukenamiki.g@gmail.com>
2022-04-09 22:56:41 +0900

    いいね数順 ( [Rails]いいね数順でランキングかつ…通り実装 )

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 100 }
