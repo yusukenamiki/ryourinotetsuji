ec568a0e10e974c3ddc472f772b2858c5feba1d7
yusukenamiki <yusukenamiki.g@gmail.com>
2022-04-13 19:38:38 +0900

    いいね数順 ( 表示件数を変更 )

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @search_recipes = @search.result.order("updated_at DESC").page(params[:page]).per(3)
+    recipes = Recipe.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+    @recipes = Kaminari.paginate_array(recipes).page(params[:page]).per(3)
   end
 end
