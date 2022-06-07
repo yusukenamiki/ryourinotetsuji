da861aacd36cff3f079ead474b00851f51292714
yusukenamiki <yusukenamiki.g@gmail.com>
2022-06-08 03:49:39 +0900

    パスワードの再設定時、「パスワードリセット用トークンは不正な値です。」とエラーが出たのを修正し解決

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = { host: 'https://www.ryourinotetsuji.com/' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   if Rails.application.credentials.gmail.present?
     mail_address = Rails.application.credentials.gmail[:address]
@@ -78,5 +78,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'https://www.ryourinotetsuji.com/' }
+  config.action_mailer.default_url_options = { host: '192.168.33.11', port: 3000 }
 end
