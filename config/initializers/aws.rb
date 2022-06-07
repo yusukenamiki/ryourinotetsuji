351bbba06de91e91f96ede90cfa1bbac1f128754
yusukenamiki <yusukenamiki.g@gmail.com>
2022-06-07 20:11:16 +0900

    記事通り設定のままだとエラーが出たのを修正し解決

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,5 +3,3 @@ creds = Aws::Credentials.new(
   ENV['AWS_SECRET_ACCESS_KEY']
 )
 Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: creds, region: 'eu-west-1')
-
-config.action_mailer.delivery_method = :aws_sdk
